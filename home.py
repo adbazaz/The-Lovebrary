17a469dfbad8abfe8c09b8facc8a979a8060fc59
Paul <paul@saito.(none)>
2009-09-13 11:26:40 -0400

    Some basic structure, beginnings

---
+++ home.py
@@ -0,0 +1,37 @@
+import os
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+
+        user = users.get_current_user()
+        if user:
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+            name = user.nickname()
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+            name = "Anonymous"
+
+        template_values = {
+          'url': url,
+          'url_linktext': url_linktext,
+          'name': name,
+          }
+
+        path = os.path.join('templates', 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
